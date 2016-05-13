452f482edb44ef4a0940d83a0809f4cca9d106ab
Luka Domitrovic <luka@domitrovic.si>
2016-05-13 12:59:18 +0200

    add Particle API call and update state in case of error

--- app/jobs/light_broadcast_job.rb
+++ app/jobs/light_broadcast_job.rb
@@ -2,6 +2,11 @@ class LightBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(light)
+    client = Particle::Client.new
+    client.device('lights').call(light.state, light.name)
+  rescue Particle::Error
+    light.update state: !light.state
+  ensure
     ActionCable.server.broadcast 'lights_channel', state: light.state, type: light.id
   end
 end
